32cc8e49e33114cef4f217dc1cf9ed62a7da0028
Gary Gan <gan.fang.yi@foxmail.com>
2024-10-18 17:36:16 +0800

    Refactor editor module and improve terminal handling

--- src/editor.rs
+++ src/editor.rs
@@ -1,8 +1,11 @@
 use crossterm::event::{read, Event::Key, KeyCode::Char, KeyEvent, KeyModifiers, Event};
-use std::io::stdout;
+use std::io::Error;
 
 mod terminal;
-use terminal::Terminal;
+use terminal::{Terminal, Position, Size};
+
+const NAME: &str = env!("CARGO_PKG_NAME");
+const VERSION: &str = env!("CARGO_PKG_VERSION");
 
 pub struct Editor {
     should_quit: bool,
@@ -20,13 +23,39 @@ impl Editor {
         result.unwrap();
     }
 
-    fn draw_rows() -> Result<(), std::io::Error> {
-        let (_, row) = Terminal::size()?;
-        for current_row in 0..row {
-            crossterm::cursor::MoveTo(0, current_row);
-            println!("~");
+    fn draw_rows() -> Result<(), Error> {
+        let Size { height, ..} = Terminal::size()?;
+        for current_row in 0..height {
+            Terminal::clear_line()?;
+            if current_row == height / 3 {
+                Self::draw_welcome_message()?;
+            } else {
+                Self::draw_empty_row()?;
+            }
+
+            if current_row + 1 < height {
+                Terminal::print("\r\n")?;
+            }
         }
 
+        Ok(())
+    }
+
+    fn draw_welcome_message() -> Result<(), Error> {
+        let mut welcome_message = format!("{NAME} editor -- version {VERSION}");
+        let width = Terminal::size()?.width as usize;
+        let len = welcome_message.len();
+        let padding = (width - len) / 2;
+        // omit some spaces for ~
+        let space = " ".repeat(padding - 1);
+        welcome_message = format!("~{space}{welcome_message}");
+        welcome_message.truncate(width);
+        Terminal::print(welcome_message)?;
+        Ok(())
+    }
+
+    fn draw_empty_row() -> Result<(), Error> {
+        Terminal::print("~")?;
         Ok(())
     }
 
@@ -53,14 +82,18 @@ impl Editor {
     }
 
     fn refresh_screen(&self) -> Result<(), std::io::Error> {
+        Terminal::hide_cursor()?;
         if self.should_quit {
             Terminal::clear_screen()?;
-            println!("Goodbye.");
+            Terminal::print("Goodbye.\r\n")?;
         } else {
             Self::draw_rows()?;
-            Terminal::move_cursor_to(0, 0)?;
+            Terminal::move_cursor_to(Position {x: 0, y: 0})?;
         }
+        Terminal::show_cursor()?;
+        Terminal::execute()?;
 
         Ok(())
     }
+
 }
--- src/editor/terminal.rs
+++ src/editor/terminal.rs
@@ -1,33 +1,80 @@
-use std::io::stdout;
+use std::io::{stdout, Error, Write};
+use crossterm::cursor::{Hide, MoveTo, Show};
+use crossterm::{queue, Command};
+use core::fmt::Display;
+use crossterm::style::Print;
+use crossterm::terminal::{disable_raw_mode, enable_raw_mode, Clear, ClearType, size};
 
-use crossterm::{cursor::MoveTo, execute, terminal::{disable_raw_mode, enable_raw_mode, Clear, ClearType}};
+#[derive(Clone, Copy)]
+pub struct Size {
+    pub height: u16,
+    pub width: u16,
+}
+
+#[derive(Clone, Copy)]
+pub struct Position {
+    pub x: u16,
+    pub y: u16,
+}
 
 pub struct Terminal {}
 
 impl Terminal {
-    pub fn initialize() -> std::io::Result<()> {
+    pub fn initialize() -> Result<(), Error> {
         enable_raw_mode()?;
         Self::clear_screen()?;
-        Self::move_cursor_to(0, 0)?;
+        Self::move_cursor_to(Position { x: 0, y: 0 })?;
+        Self::execute()?;
         Ok(())
     }
 
-    pub fn terminate() -> std::io::Result<()> {
+    pub fn terminate() -> Result<(), Error> {
         disable_raw_mode()?;
         Ok(())
     }
 
-    pub fn clear_screen() -> std::io::Result<()> {
-        execute!(stdout(), Clear(ClearType::All))?;
+    pub fn clear_screen() -> Result<(), Error> {
+        Self::queue_command(Clear(ClearType::All))?;
         Ok(())
     }
 
-    pub fn move_cursor_to(x: u16, y: u16) -> std::io::Result<()> {
-        execute!(stdout(), MoveTo(x, y))?;
+    pub fn clear_line() -> Result<(), Error> {
+        Self::queue_command(Clear(ClearType::CurrentLine))?;
         Ok(())
     }
 
-    pub fn size() -> std::io::Result<(u16, u16)> {
-        crossterm::terminal::size()
+    pub fn move_cursor_to(position: Position) -> Result<(), Error> {
+        Self::queue_command(MoveTo(position.x, position.y))?;
+        Ok(())
+    }
+
+    pub fn hide_cursor() -> Result<(), Error> {
+        Self::queue_command(Hide)?;
+        Ok(())
+    }
+
+    pub fn show_cursor() -> Result<(), Error> {
+        Self::queue_command(Show)?;
+        Ok(())
+    }
+
+    pub fn execute() -> Result<(), Error> {
+        stdout().flush()?;
+        Ok(())
+    }
+
+    pub fn size() -> Result<Size, Error> {
+        let (width, height) = size()?;
+        Ok(Size { height, width })
+    }
+
+    pub fn print<T: Display>(string: T) -> Result<(), Error> {
+        Self::queue_command(Print(string))?;
+        Ok(())
+    }
+
+    fn queue_command<T: Command>(command: T) -> Result<(), Error> {
+        queue!(stdout(), command)?;
+        Ok(())
     }
 }
